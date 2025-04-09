aca149fc3d754f56df3791d6942eb50fa4e39ed4
tienmanh2k1 <93610981+tienmanh2k1@users.noreply.github.com>
2025-04-09 13:29:43 +0700

    Add asics-theme/functions.php 1744180183440

---
+++ asics-theme/functions.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * ASICS Vietnam Theme functions and definitions
+ */
+
+function asics_theme_setup() {
+    // Add theme support
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ));
+    
+    // Register navigation menus
+    register_nav_menus(array(
+        'primary' => __('Primary Menu', 'asics'),
+        'footer' => __('Footer Menu', 'asics')
+    ));
+}
+add_action('after_setup_theme', 'asics_theme_setup');
+
+// Enqueue styles and scripts
+function asics_theme_scripts() {
+    // Theme stylesheet
+    wp_enqueue_style('asics-style', get_stylesheet_uri());
+    
+    // Google Fonts
+    wp_enqueue_style('asics-fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap');
+    
+    // Font Awesome
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css');
+}
+add_action('wp_enqueue_scripts', 'asics_theme_scripts');
